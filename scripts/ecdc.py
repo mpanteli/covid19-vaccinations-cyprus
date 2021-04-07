cd62038fefa8f2ca1b84939cf3c56eedc76616b5
Maria Panteli <panteli_maria@hotmail.com>
2021-04-07 22:33:01 +0200

    Fix formatting

--- scripts/ecdc.py
+++ scripts/ecdc.py
@@ -1,8 +1,8 @@
 import argparse
 from datetime import datetime
 
-import pycountry
 import pandas as pd
+import pycountry
 
 VACCINE_MAPPER = {
     'COM': 'Pfizer/BioNTech',
@@ -11,8 +11,7 @@ VACCINE_MAPPER = {
     'SIN': 'Sinovac',
     'SPU': 'Sputnik V',
     'AZ': 'AstraZeneca',
-    'UNK': 'UNKNOWN'
-}
+    'UNK': 'UNKNOWN'}
 CSV_URL = ('https://opendata.ecdc.europa.eu/'
            'covid19/vaccine_tracker/csv/data.csv')
 SOURCE_URL = ('https://www.ecdc.europa.eu/en/publications-data/'
@@ -26,44 +25,37 @@ def filter_data(df: pd.DataFrame, country: str) -> pd.DataFrame:
 
     df_sub.loc[:, 'people_vaccinated'] = df_sub['FirstDose'].cumsum()
     df_sub.loc[:, 'people_fully_vaccinated'] = df_sub['SecondDose'].cumsum()
-
-    total = df_sub['people_vaccinated'] + df_sub['people_fully_vaccinated']
-    df_sub.loc[:, 'total_vaccinations'] = total
+    df_sub.loc[:, 'total_vaccinations'] = (
+        df_sub['people_vaccinated'] + df_sub['people_fully_vaccinated'])
 
     df_sub.loc[:, 'date'] = [
-        min(
-            datetime.strptime(week + '-7', '%G-W%V-%u').date(),
-            datetime.today().date()) for week in df_sub['YearWeekISO']
-    ]
+        min(datetime.strptime(week + '-7', '%G-W%V-%u').date(),
+            datetime.today().date()) for week in df_sub['YearWeekISO']]
     df_sub.loc[:, 'total_received'] = (
         df_sub['NumberDosesReceived'].cumsum().astype('int'))
+    
     df_sub.loc[:, 'location'] = pycountry.countries.get(alpha_2=country).name
     df_sub.loc[:, 'vaccine'] = [
-        VACCINE_MAPPER.get(vaccine, 'UNKNOWN') for vaccine in df_sub['Vaccine']
-    ]
+        VACCINE_MAPPER.get(vac, 'UNKNOWN') for vac in df_sub['Vaccine']]
     df_sub.loc[:, 'source_url'] = SOURCE_URL
     df_filtered = df_sub[[
         'location', 'date', 'vaccine', 'source_url', 'total_vaccinations',
-        'people_vaccinated', 'people_fully_vaccinated', 'total_received'
-    ]]
+        'people_vaccinated', 'people_fully_vaccinated', 'total_received']]
     return df_filtered
 
 
 def aggregate_data(df: pd.DataFrame) -> pd.DataFrame:
     """ Aggregate data per week """
-    df_agg = df.groupby(['location', 'date', 'source_url']).agg({
-        'vaccine':
-        lambda x: ','.join(sorted(x.unique())),
-        'total_vaccinations':
-        'max',
-        'people_vaccinated':
-        'max',
-        'people_fully_vaccinated':
-        'max',
-        'total_received':
-        'max'
-    })
-    return df_agg.reset_index()
+    df_agg = (df
+        .groupby(['location', 'date', 'source_url'])
+        .agg({
+            'vaccine': lambda x: ','.join(sorted(x.unique())),
+            'total_vaccinations': 'max',
+            'people_vaccinated': 'max',
+            'people_fully_vaccinated': 'max',
+            'total_received': 'max'})
+        .reset_index())
+    return df_agg
 
 
 def main(country_name: str, output_path: str) -> None:
